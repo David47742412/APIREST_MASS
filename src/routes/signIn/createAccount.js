68e67772d634d8b64bfd8523a30d4456bca7f3a2
David47742412 <davidcastroastudillo20@gmail.com>
2022-11-29 08:54:08 -0500

    as

--- src/routes/signIn/createAccount.js
+++ src/routes/signIn/createAccount.js
@@ -6,7 +6,7 @@ const helpers = require('../../lib/helpers');
 
 router.post('/', async (req, res)=>{
     try {
-        const { usuario_nombre, usuario_apellido, usuario_numero, usuario_email, usuario_password, usuario_token, usuario_create, usuario_update } = req.body;
+        const { usuario_nombre, usuario_apellido, usuario_numero, usuario_email, usuario_password, usuario_token, usuario_create, usuario_update, usuario_foto } = req.body;
         const user = {
             usuario_nombre,
             usuario_apellido,
@@ -16,9 +16,12 @@ router.post('/', async (req, res)=>{
             usuario_token,
             usuario_create,
             usuario_update,
+            usuario_foto
         }
 
         user.usuario_create = await helpers.getDateNow();
+
+        user.usuario_foto = await "https://i.ibb.co/YThSnKy/vector-de-perfil-avatar-predeterminado-foto-usuario-medios-sociales-icono-183042379.jpg"
         
         user.usuario_update = await helpers.getDateNow();
 
@@ -28,10 +31,9 @@ router.post('/', async (req, res)=>{
     
         user.usuario_token = await helpers.getToken(user);
 
-    
         await pool.query('INSERT INTO usuario set ?', [user]);
     
-        res.send("user");
+        res.send("Usuario Creado satisfactoriamente");
         
     } catch (error) {
 
